d6c4318f39ea403a52cb103accb222b317d3d3e6
valeriarc09 <valeriarengifo09@gmail.com>
2021-09-15 09:12:48 -0500

    trabajando

--- Backend/src/routes/admin.js
+++ Backend/src/routes/admin.js
@@ -2,20 +2,47 @@ const express = require('express');
 const router = express.Router();
 
 //llamnado controlador
-const {getAdmins, getAdmin, createProfesor} = require('../controllers/admin.controllers');
+const {getAdmins, getAdmin, createProfesor, createPersona, createEstudiante, createPrograma, 
+    createAdministrador, createAsignatura, editPersona, editAsignatura, editPrograma} = require('../controllers/admin.controllers');
 
-//Esta ruta hace referencia a /api/admin en app.js
-
+//Rutas para obtener datos (Tipo GET)
 router.route('/')
     .get(getAdmins); 
-
 
 router.route('/:id')
     .get(getAdmin); 
 
-router.route('/crearProfesor')
-    .post(createProfesor); 
+//Rutas para crear datos (Insertar en tablas) (Tipo POST)
+router.route('/crearPersona')
+    .post(createPersona)
 
+router.route('/crearProfesor')
+    .post(createProfesor)
+
+router.route('/crearEstudiante')
+    .post(createEstudiante)
+
+router.route('/crearAdministrador')
+    .post(createAdministrador)
+
+router.route('/crearPrograma')
+    .post(createPrograma)
+
+router.route('/crearAsignatura')
+    .post(createAsignatura)
+
+
+//Rutas para editar datos (Tipo PUT)    
+router.route('/editPersona/:id')
+    .put(editPersona)
+
+
+router.route('/editPrograma/:id')
+    .put(editPrograma)
+
+
+router.route('/editAsignatura/:id')
+    .put(editAsignatura)
 
 
 module.exports = router;
